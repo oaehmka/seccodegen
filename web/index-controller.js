8fc2f260dd6a07195c0f3e65fc0b7026da32412c
Fabio Gabrielli <fabio.gabrielli@students.fhnw.ch>
2024-05-14 22:37:41 +0200

    working "generateDataset"

--- web/index-controller.js
+++ web/index-controller.js
@@ -17,23 +17,29 @@ exports.generate = async (req, res) => {
   // #swagger.tags = ['index']
   logger.debug("generate called");
 
+  const response = {"code": await generateCode(req.body.prompt)}
+
+  res.status(201).json(response);
+};
+
+async function generateCode(prompt) {
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: req.body.prompt }],
+      messages: [{ role: "user", content: prompt }],
       temperature: 0.7,
     }),
     headers: {
       "Content-type": "application/json; charset UTF-8;",
       Authorization: "Bearer " + process.env.OPENAI_API_KEY,
     },
-  }).then((response) => response.json());
+  }).then((r) => r.json());
 
-  res.status(201).json(response.choices[0].message.content);
-};
+  return await response.choices[0].message.content;
+}
 
-exports.enrich = async (req, res) => {
+exports.enrich = (req, res) => {
   // #swagger.tags = ['index']
   logger.debug("enrichDataset called");
 
@@ -52,17 +58,24 @@ function enrichDataset(prefix, suffix) {
     element.prompt = p + element.prompt + s;
   });
 
-  return dataset_copy
+  return dataset_copy;
 }
 
 exports.generateDataset = async (req, res) => {
   // #swagger.tags = ['index']
   logger.debug("generateDataset called");
 
-  const set = enrichDataset(req.body.prefix, req.body.suffix);
+  const prompts = enrichDataset(req.body.prefix, req.body.suffix);
 
-  // TODO: generate code for each prompt
-  set.forEach((el) => console.log(el));
+  let re = [];
+  await Promise.all(
+    prompts.map(async (element) => {
+      re.push({"code": await generateCode(element.prompt)});
+    })
+  );
 
-  res.status(200).json(set);
+  console.log(re);
+  console.log(await Promise.all(re));
+
+  res.status(200).json(re);
 };
